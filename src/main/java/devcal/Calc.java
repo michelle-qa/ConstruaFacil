c2035b089370934394953856071427eec39b7488
michlle-qa <michellesilva.oab@gmail.com>
2021-06-11 23:00:32 -0300

    Exercício TFS130 Novo Projeto  Calc.java

---
+++ src/main/java/devcal/Calc.java
@@ -0,0 +1,22 @@
+// 1 - Pacote <endereço; onde que fica>
+package devcal;
+// 2 - Bibliotecas <o que ele recebe dos outros programas>
+
+// 3 - Classes < quem é o JOão>
+public class Calc {
+    // 3.1 - Atributos <como ele é>
+
+    // 3.2 - Métodos e funções <são as ações >
+    //os Métodos podem ser Públicos: que maria pode ver o que João faz. E
+    //Privado ninguém pode ver apenas o João
+    public static void main(String[] args){
+        System.out.print("Passou pelo método main");
+    }
+
+public static void somarDoisNumeros(){
+    System.out.println("Passou pelo método somarDoisNumeros");
+
+
+    }
+
+}
